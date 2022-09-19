f914301cf8b4a59a950c1a715e4a5426225cc6c2
Mihai Fekete <feketemihai@nexterp.ro>
2022-09-19 21:07:29 +0300

    Add module for auto update prices in sales.

---
+++ nexterp_sale_update_price_auto/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2022 NextERP Romania SRL
+# License OPL-1.0 or later
+# (https://www.odoo.com/documentation/user/14.0/legal/licenses/licenses.html#).
+
+{
+    "name": "NextERP - Sale Update Prices Auto",
+    "version": "14.0.1.0.1",
+    "depends": ["sale_management"],
+    "description": """NextERP - Sale Update Prices Auto""",
+    "author": "NextERP Romania",
+    "website": "https://nexterp.ro",
+    "support": "contact@nexterp.ro",
+    "data": [
+        # views
+        "views/res_config_settings_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "development_status": "Mature",
+    "maintainers": ["feketemihai"],
+    "license": "OPL-1",
+}
