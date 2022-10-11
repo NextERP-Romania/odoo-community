dacce51ea974a4b7486f93affab118410f93f0db
Stana Natanaela <musatnatanaela@gmail.com>
2022-10-11 11:26:43 +0300

    add nexterp_account_invoice_report

---
+++ nexterp_account_invoice_report/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2022 NextERP Romania SRL
+# License OPL-1.0 or later
+# (https://www.odoo.com/documentation/user/14.0/legal/licenses/licenses.html#).
+
+{
+    "name": "NextERP - Account Invoice Report",
+    "summary": """NextERP - Account Invoice Report""",
+    "version": "14.0.1.0.1",
+    "category": "Invoicing",
+    "depends": ["account"],
+    "data": ["views/account_invoice_report.xml"],
+    "author": "NextERP Romania",
+    "website": "https://nexterp.ro",
+    "support": "odoo_apps@nexterp.ro",
+    "installable": True,
+    "auto_install": False,
+    "development_status": "Mature",
+    "maintainers": ["feketemihai"],
+    "license": "OPL-1",
+}
