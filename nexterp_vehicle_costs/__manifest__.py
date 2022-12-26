664be313e75d153784487c5b97d504e4c24a98a8
Mihai Fekete <feketemihai@nexterp.ro>
2022-12-26 14:58:20 +0200

    Add module for vehicle costs

---
+++ nexterp_vehicle_costs/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2016 Forest AND Biomass Romania SA
+# Copyright (C) 2021 NextERP Romania SRL
+# License OPL-1.0 or later
+# (https://www.odoo.com/documentation/user/14.0/legal/licenses/licenses.html#).
+
+{
+    "name": "NextERP - Vehicle Cost Management",
+    "version": "14.0.1.0.0",
+    "category": "Managing vehicles and contracts",
+    "depends": ["fleet", "l10n_ro_nondeductible_vat"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/fleet_view.xml",
+        "views/stock_view.xml",
+        "views/invoice_view.xml",
+        "views/product_view.xml",
+    ],
+    "author": "NextERP Romania SRL",
+    "website": "https://nexterp.ro",
+    "support": "odooapps@nexterp.ro",
+    "installable": True,
+    "auto_install": False,
+    "development_status": "Mature",
+    "maintainers": ["feketemihai"],
+    "license": "OPL-1",
+}
