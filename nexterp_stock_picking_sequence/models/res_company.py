32814392b8187749c1b2b7e1ed41b0601bac6d19
Mihai Fekete <feketemihai@nexterp.ro>
2023-01-04 17:06:56 +0200

    Add module for stock picking numbering at confirmation

---
+++ nexterp_stock_picking_sequence/models/res_company.py
@@ -0,0 +1,13 @@
+# Copyright 2022 NextERP Romania SRL
+# License OPL-1.0 or later
+# (https://www.odoo.com/documentation/user/14.0/legal/licenses/licenses.html#).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    stock_assign_number_in_process = fields.Boolean(
+        string="Assign Picking Number in Action Done"
+    )
