a75a7c53fc49194c5aecaa3bc162a4d8c9f976fd
Mihai Fekete <feketemihai@nexterp.ro>
2023-01-18 14:22:06 +0200

    Use draft also in backprders.

--- nexterp_stock_picking_sequence/models/stock_picking.py
+++ nexterp_stock_picking_sequence/models/stock_picking.py
@@ -3,7 +3,7 @@
 # (https://www.odoo.com/documentation/user/14.0/legal/licenses/licenses.html#).
 
 
-from odoo import api, models, fields
+from odoo import api, fields, models
 
 
 class StockPicking(models.Model):
@@ -18,13 +18,29 @@ class StockPicking(models.Model):
         return defaults
 
     def _action_done(self):
-        """Assign number to pickings that are from a company with this setting."""
+        """Assign number to pickings that are from a company with this setting.
+
+        Assign a number to pickings that are from a company with the setting
+        stock_assign_number_in_process enabled and that have a name that starts
+        with "Draft-" or is equal to "/". This is done by calling the next_by_id
+        method of the sequence defined in the picking type.
+        """
         self._check_company()
         for picking in self:
             if (
                 picking.picking_type_id.sequence_id
                 and picking.company_id.stock_assign_number_in_process
-                and picking.name.startswith("Draft-")
+                and (picking.name.startswith("Draft-") or picking.name == "/")
             ):
                 picking.name = picking.picking_type_id.sequence_id.next_by_id()
         return super()._action_done()
+
+    def _create_backorder(self):
+        backorders = super()._create_backorder()
+        for picking in backorders:
+            if (
+                picking.name == "/"
+                and picking.company_id.stock_assign_number_in_process
+            ):
+                picking.name = f"Draft-{fields.Datetime.now().timestamp()}"
+        return backorders
