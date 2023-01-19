2581aee0074303166c6fc26456162764c8e487e6
Stana Natanaela <musatnatanaela@gmail.com>
2023-01-19 16:43:59 +0200

    adaugare nexterp_purchase_exception si nexterp_stock_exception

---
+++ nexterp_purchase_exception/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2022 NextERP Romania SRL
+# License OPL-1.0 or later
+# (https://www.odoo.com/documentation/user/14.0/legal/licenses/licenses.html#).
+
+{
+    "name": "Nexterp Purchase Exception",
+    "summary": "Custom exceptions on purchase order",
+    "version": "14.0.1.0.0",
+    "category": "Generic Modules/Purchase",
+    "author": "NextERP Romania",
+    "website": "https://nexterp.ro",
+    "depends": ["purchase_exception"],
+    "license": "OPL-1",
+    "data": [
+        "views/purchase_view.xml",
+    ],
+    "installable": True,
+}
---
+++ nexterp_purchase_exception/models/purchase_line.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2022 NextERP Romania SRL
+# License OPL-1.0 or later
+# (https://www.odoo.com/documentation/user/14.0/legal/licenses/licenses.html#).
+
+from odoo import api, fields, models
+import html
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = ["purchase.order.line", "base.exception.method"]
+    _name = "purchase.order.line"
+
+    exception_ids = fields.Many2many(
+        "exception.rule", string="Exceptions", copy=False, readonly=True
+    )
+    exceptions_summary = fields.Html(
+        readonly=True, compute="_compute_exceptions_summary"
+    )
+
+    @api.depends("exception_ids", "ignore_exception")
+    def _compute_exceptions_summary(self):
+        for rec in self:
+            if rec.exception_ids and not rec.ignore_exception:
+                rec.exceptions_summary = rec._get_exception_summary()
+            else:
+                rec.exceptions_summary = False
+
+    def _get_exception_summary(self):
+        return "<ul>%s</ul>" % "".join(
+            [
+                "<li>%s: <i>%s</i></li>"
+                % tuple(map(html.escape, (e.name, e.description)))
+                for e in self.exception_ids
+            ]
+        )
+
+    def _detect_exceptions(self, rule):
+        records = super()._detect_exceptions(rule)
+        if records:
+            self.exception_ids = [(4, rule.id)]
+        return records
