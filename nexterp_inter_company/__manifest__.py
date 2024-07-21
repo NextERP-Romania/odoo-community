8080c46d827d7f801aa6e4b41aa0d7809d91fcf9
Mihai Fekete <feketemihai@gmail.com>
2024-07-21 13:18:22 +0300

    [MIG] nexterp_inter_company: Migration to 17.0

--- nexterp_inter_company/__manifest__.py
+++ nexterp_inter_company/__manifest__.py
@@ -7,7 +7,7 @@
     "summary": """
         This module helps to identify if a record is inter
         company transaction or not.""",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Base",
     "depends": ["base"],
     "author": "NextERP Romania",
