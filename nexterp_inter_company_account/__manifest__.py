bdd4fc23bd37a7c4bdece2668a8087e7750fa6ce
Mihai Fekete <feketemihai@gmail.com>
2024-07-21 13:18:22 +0300

    [MIG] nexterp_inter_company_account: Migration to 17.0

--- nexterp_inter_company_account/__manifest__.py
+++ nexterp_inter_company_account/__manifest__.py
@@ -7,7 +7,7 @@
     "summary": """
         This module helps to identify if an account move line
         and account move is inter company transaction or not.""",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Accounting",
     "depends": ["nexterp_inter_company", "account"],
     "author": "NextERP Romania",
