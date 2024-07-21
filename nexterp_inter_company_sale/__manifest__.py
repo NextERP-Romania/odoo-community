0fc59a8da08ff68b5382ac80d51ee09c1c427f3d
Mihai Fekete <feketemihai@gmail.com>
2024-07-21 13:18:22 +0300

    [MIG] nexterp_inter_company_sale: Migration to 17.0

--- nexterp_inter_company_sale/__manifest__.py
+++ nexterp_inter_company_sale/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "NextERP - Inter Company Sale",
     "summary": """This module helps to identify if an sale order line """
     """and sale order is inter company transaction or not.""",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Sales",
     "depends": ["nexterp_inter_company", "sale"],
     "author": "NextERP Romania",
