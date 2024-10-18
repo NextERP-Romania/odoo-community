a489b72c7756854b025f01d6bf079ea193caea2b
Marius <marius.nick@gmail.com>
2024-10-18 13:40:21 +0300

    imporove module nexterp_account_edi_ubl_extracode

--- nexterp_account_edi_ubl_extracode/models/account_edi_cius.py
+++ nexterp_account_edi_ubl_extracode/models/account_edi_cius.py
@@ -23,23 +23,31 @@ class AccountEdiXmlCIUSRO(models.Model):
 
     def _get_delivery_vals_list(self, invoice):
         res = super()._get_delivery_vals_list(invoice)
-
-        shipping_address = False
-        if "partner_shipping_id" in invoice._fields and invoice.partner_shipping_id:
-            shipping_address = invoice.partner_shipping_id
-            if shipping_address == invoice.partner_id:
-                shipping_address = False
-        if shipping_address:
-            res = [
-                {
-                    "actual_delivery_date": invoice.invoice_date,
-                    "delivery_location_vals": {
-                        "delivery_address_vals": self._get_partner_address_vals(
-                            shipping_address
-                        ),
-                    },
-                    "id": shipping_address.gln,
-                }
-            ]
+        gln_module = (
+                    self.env["ir.module.module"]
+                    .sudo()
+                    .search(
+                        [("name", "=", "deltatech_gln"), ("state", "=", "installed")],
+                        limit=1,
+                    )
+                )
+        if gln_module:
+            shipping_address = False
+            if "partner_shipping_id" in invoice._fields and invoice.partner_shipping_id:
+                shipping_address = invoice.partner_shipping_id
+                if shipping_address == invoice.partner_id:
+                    shipping_address = False
+            if shipping_address:
+                res = [
+                    {
+                        "actual_delivery_date": invoice.invoice_date,
+                        "delivery_location_vals": {
+                            "delivery_address_vals": self._get_partner_address_vals(
+                                shipping_address
+                            ),
+                        },
+                        "id": shipping_address.gln,
+                    }
+                ]
 
         return res
