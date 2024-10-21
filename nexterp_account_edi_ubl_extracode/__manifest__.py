2bfb7cd44792699228621597fdb2b16a0057ccf8
Marius <marius.nick@gmail.com>
2024-10-21 13:02:16 +0300

    fix  StandardIdentificator Schema

--- nexterp_account_edi_ubl_extracode/__manifest__.py
+++ nexterp_account_edi_ubl_extracode/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "NextERP - Account UBL Extra Code",
     "summary": """NextERP - Adds additional codes to e-factura such as delivery slip number,
     customer item code,GLN code and EAN cod""",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.2",
     "category": "Invoicing",
     "depends": ["account", "l10n_ro_account_edi_ubl"],
     "data": ["views/ubl_template.xml"],
