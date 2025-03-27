3e25a9e019e8eed1560058d7450a97de9f9e46c1
Sima Elisabeta <elisabeta.sima@nexterp.ro>
2025-03-27 13:11:14 +0200

    18.0-mig-l10n_ro_account_edi_cpv

---
+++ l10n_ro_account_edi_cpv/models/l10n_ro_cpv_code.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2025 NextERP Romania
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class L10nROCPVCode(models.Model):
+    _name = "l10n.ro.cpv.code"
+    _description = "L10n Romania CPV Code"
+
+    name = fields.Char()
+    code = fields.Char()
