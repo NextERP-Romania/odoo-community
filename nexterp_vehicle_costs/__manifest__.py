bb34f77d15111d80ea67e5676d13d4aa7aa0fa91
Marius <marius.nick@gmail.com>
2025-03-28 10:22:29 +0200

    [MIG] nexterp_vehicle_costs to  18.0

--- nexterp_vehicle_costs/__manifest__.py
+++ nexterp_vehicle_costs/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright (C) 2016 Forest AND Biomass Romania SA
-# Copyright (C) 2024 NextERP Romania SRL
+# Copyright (C) 2025 NextERP Romania SRL
 # License AGPL-3.0 or later
 # (https://www.odoo.com/documentation/user/14.0/legal/licenses/licenses.html#).
 
 {
     "name": "NextERP - Vehicle Cost Management",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Managing vehicles and contracts",
     "depends": ["fleet", "account_fleet", "l10n_ro_nondeductible_vat", "stock"],
     "summary": "Manage vehicle costs, add categories for fuel and part, etc.",
