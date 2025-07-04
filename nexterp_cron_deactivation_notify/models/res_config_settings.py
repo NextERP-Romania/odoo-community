11f61b5f8da444474d742a00b23a36f9151db602
Sima Elisabeta <elisabeta.sima@nexterp.ro>
2025-07-04 11:36:32 +0300

    [MIG]-nexterp_cron_deactivation_notify: Migration to 18.0

---
+++ nexterp_cron_deactivation_notify/models/res_config_settings.py
@@ -0,0 +1,15 @@
+# Copyright 2022 NextERP Romania SRL
+# License OPL-1.0 or later
+# (https://www.odoo.com/documentation/user/16.0/legal/licenses/licenses.html#).
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    archived_cron_notify_users_ids = fields.Many2many(
+        string="Users to Notify for cron deactivation",
+        related="company_id.archived_cron_notify_users_ids",
+        readonly=False,
+    )
