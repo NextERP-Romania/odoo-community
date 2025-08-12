ce7b77e03b522d91bc29ba4e1adc768c95cdb74e
stananatanaela <musatnatanaela@gamil.com>
2025-08-12 16:28:27 +0300

    add nexterp_stock_exception

---
+++ nexterp_stock_exception/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2022 NextERP Romania SRL
+# License AGPL-3.0 or later
+# (https://www.odoo.com/documentation/user/15.0/legal/licenses/licenses.html#).
+
+{
+    "name": "NextERP - Stock Exception",
+    "summary": "Custom exceptions on stock move",
+    "version": "18.0.0.0.0",
+    "category": "Generic Modules/Stock",
+    "author": "NextERP Romania",
+    "website": "https://www.nexterp.ro",
+    "depends": ["stock_exception"],
+    "license": "AGPL-3",
+    "data": [
+        "views/stock_view.xml",
+    ],
+    "installable": True,
+    "maintainers": ["feketemihai"],
+    "development_status": "Mature",
+}
