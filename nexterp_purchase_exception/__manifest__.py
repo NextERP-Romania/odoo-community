6ad0fa630c249f30b73e036e1cba34f128c2646a
stananatanaela <musatnatanaela@gamil.com>
2025-08-12 16:09:18 +0300

    add nexterp_purchase_exception

---
+++ nexterp_purchase_exception/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2022 NextERP Romania SRL
+# License AGPL-3.0 or later
+# (https://www.odoo.com/documentation/user/15.0/legal/licenses/licenses.html#).
+
+{
+    "name": "NextERP - Purchase Exception",
+    "summary": "Custom exceptions on purchase order line",
+    "version": "18.0.0.0.0",
+    "category": "Generic Modules/Purchase",
+    "author": "NextERP Romania",
+    "website": "https://www.nexterp.ro",
+    "depends": ["purchase_exception"],
+    "license": "AGPL-3",
+    "data": [
+        "views/purchase_view.xml",
+    ],
+    "installable": True,
+    "maintainers": ["feketemihai"],
+    "development_status": "Mature",
+}
---
+++ nexterp_purchase_exception/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+- This module allows you to see exception also on purchase order line.
