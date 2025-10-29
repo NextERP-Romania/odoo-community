994372f895d54dfe9cd7c0d4209d228928f42516
stananatanaela <musatnatanaela@gamil.com>
2025-10-29 12:17:40 +0200

    add nexterp_account_payment_invoice

---
+++ nexterp_account_payment_invoice/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 NextERP Romania
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Nexterp - Payment Invoice",
+    "summary": "Nexterp - Payment Invoice",
+    "version": "17.0.1.0.0",
+    "category": "Localization",
+    "author": "NextERP Romania SRL",
+    "website": "https://nexterp.ro",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+       'account'
+    ],
+    "data": [
+       
+    ],
+}
