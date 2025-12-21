5603cc38934e235b69d7255fcf4a4c8106875b2a
Mihai Fekete <feketemihai@nexterp.ro>
2025-12-21 12:27:32 +0200

    Update invoice report, remove tax name conditional

--- nexterp_account_edi_journal/__manifest__.py
+++ nexterp_account_edi_journal/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "NextERP - Account EDI Journalt",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "summary": """ NextERP - Account EDI Journal""",
     "category": "Accounting",
     "author": "NextERP Romania",
--- nexterp_account_edi_journal/models/account_journal.py
+++ nexterp_account_edi_journal/models/account_journal.py
@@ -7,7 +7,7 @@ from odoo import models, fields
 class AccountJournal(models.Model):
     _inherit = "account.journal"
 
-    l10n_ro_edi_send_enabled = models.Boolean(
+    l10n_ro_edi_send_enabled = fields.Boolean(
         string="Enable EDI Send",
         help="Enable sending this journal entry as an EDI document.",
         default=False,
