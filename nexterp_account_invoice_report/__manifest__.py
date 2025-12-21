41e785ad6c83a133b5c040b5db9d05219664bb64
Mihai Fekete <feketemihai@nexterp.ro>
2025-12-21 12:39:36 +0200

    Update invoice report, remove tax name conditional

--- nexterp_account_invoice_report/__manifest__.py
+++ nexterp_account_invoice_report/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "NextERP - Account Invoice Report",
-    "version": "16.0.1.0.1",
+    "version": "16.0.1.0.2",
     "summary": """ NextERP - Account Invoice Report""",
     "category": "Accounting",
     "author": "NextERP Romania",
