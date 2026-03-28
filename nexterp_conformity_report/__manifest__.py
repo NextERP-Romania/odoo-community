50387caa69b11560ead76842744f27de21237f59
Mihai Fekete <feketemihai@nexterp.ro>
2026-03-28 12:39:38 +0200

    Migrate conformity report

---
+++ nexterp_conformity_report/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2022 NextERP Romania SRL
+# License AGPL-3.0 or later
+{
+    "name": "NextERP - Conformity Certificate",
+    "version": "19.0.1.0.0",
+    "author": "NextERP Romania",
+    "website": "https://www.nexterp.ro",
+    "category": "Special",
+    "depends": ["sale_stock", "l10n_ro_stock_picking_comment_template"],
+    "data": ["report/certificat_conformitate.xml"],
+    "auto_install": False,
+    "installable": True,
+    "license": "AGPL-3",
+}
