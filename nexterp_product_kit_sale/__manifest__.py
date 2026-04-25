3beef87f196b1fc0018073b40e1a53dd98f10bf6
Mihai Fekete <feketemihai@nexterp.ro>
2026-04-25 17:26:19 +0300

    Migrate modules

---
+++ nexterp_product_kit_sale/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2022 NextERP Romania SRL
+# License OPL-1.0 or later
+# (https://www.odoo.com/documentation/user/19.0/legal/licenses/licenses.html#).
+
+{
+    "name": "NextERP - Product Kit Sale",
+    "summary": "NextERP - Product Kit Sale",
+    "version": "19.0.1.0.0",
+    "category": "Sales",
+    "depends": ["nexterp_product_kit", "sale"],
+    "author": "NextERP Romania",
+    "website": "https://github.com/NextERP-Romania/odoo-community",
+    "support": "odoo_apps@nexterp.ro",
+    "data": [
+        # views
+        "views/sale_order_views.xml",
+        # security
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "development_status": "Mature",
+    "maintainers": ["feketemihai"],
+    "images": ["static/description/apps_icon.png"],
+    "license": "OPL-1",
+}
---
+++ nexterp_product_kit_sale_timesheet/models/sale_order.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2022 NextERP Romania SRL
+# License OPL-1.0 or later
+# (https://www.odoo.com/documentation/user/19.0/legal/licenses/licenses.html#).
+
+from odoo import models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def _compute_tasks_ids(self):
+        for order in self:
+            for line in order.kit_line_ids:
+                if line.task_id and not line.task_id.sale_line_id:
+                    line.task_id.sale_line_id = line.sale_line_id
+        return super()._compute_tasks_ids()
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    def _timesheet_create_task(self, project):
+        """Generate task for each kit line"""
+        res = super()._timesheet_create_task(project)
+        if self.product_id.service_tracking == "task_in_project":
+            if not project:
+                project = self.task_id.project_id
+            for kit_line in self.kit_line_ids:
+                if kit_line.product_id.service_tracking == "task_in_project":
+                    if not kit_line.task_id and kit_line.sale_line_id:
+                        kit_line.with_context(parent_task=res)._timesheet_create_task(
+                            project
+                        )
+        return res
